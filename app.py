ba5abea5bd1ecae43ea608b1730479253efba130
darthnall <blake.nall21@gmail.com>
2023-08-23 17:45:57 -0500

    Restructured project

--- app.py
+++ app.py
@@ -1,30 +1,11 @@
 from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
 
-cpus = [
-        {
-            'name': 'i5-4690K',
-            'generation': '4',
-            'manufacturer': 'Intel',
-            'purchased_by': 'blake',
-            'qty': '1',
-        }
-]
-
-
-@app.route("/")
-@app.route("/home")
-@app.route("/dashboard")
-def dash():
-    return render_template('dash.html', cpus=cpus, title='dashboard')
-
-
-@app.route("/report")
-def report():
-    return render_template('report.html', title='report')
+@app.route('/')
+def home():
+    return "Home Screen"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
