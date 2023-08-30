c572ea5cdfb7f22be2240448bda971f3a4ed1816
Blake Nall <blakenall@pm.me>
2023-08-29 21:00:38 -0500

    Rendering html properly

--- arkinventory/__init__.py
+++ arkinventory/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .db.database import db
+import os
 
 def create_app():
-    app = Flask(__name__)
+    print("Template folder path:", os.path.abspath('./templates'))
+    app = Flask(__name__, template_folder=os.path.abspath('./templates'))
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
     db.init_app(app)
+
+    from arkinventory.client.routes import client_bp
+    app.register_blueprint(client_bp, url_prefix='')
 
     with app.app_context():
         db.create_all()
