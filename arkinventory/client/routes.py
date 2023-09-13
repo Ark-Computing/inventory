c938231e18e086983318476b8a637cf9a70cdded
Blake Nall <blakenall@proton.me>
2023-09-13 18:14:16 -0500

    Added report route

--- arkinventory/client/routes.py
+++ arkinventory/client/routes.py
@@ -4,4 +4,8 @@ client_bp = Blueprint('client', __name__)
 
 @client_bp.route('/')
 def home():
-    return render_template('layout.html')
+    return render_template('dash.html', title="Dashboard")
+
+@client_bp.route('/report')
+def report():
+    return render_template('report.html', title="Report")
